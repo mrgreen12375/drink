fd4a8e79b68220a68ca97e0735fc3a26226d52a7
Peter Holcomb <pholcomb91@gmail.com>
2022-09-15 11:34:33 -0700

    feat: dynamically creating a list of saved drinks

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 var i=0
 function createFavoriteList() {
-    var drink=document.getElementById("drinksSaved");
+    var drink=document.getElementById("drinkSaved");
+    var saveHistory=document.createElement("li");
+    saveHistory.classList.add("listStyle");
     localStorage.setItem("drink"+i, drink.value)
     i=i+1
 }
