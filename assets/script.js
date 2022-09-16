5ae109412632f69787c617d18444fc1188d9f520
Peter Holcomb <pholcomb91@gmail.com>
2022-09-15 18:51:26 -0700

    feat: saved drinks are displayed

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,14 @@ function createFavoriteList() {
     var drink=document.getElementById("drinkSaved");
     var saveHistory=document.createElement("li");
     saveHistory.classList.add("listStyle");
-    localStorage.setItem("drink"+i, drink.value)
+    localStorage.setItem("drink"+i, drink.value);
+    saveHistory.innerHTML=drink.value;
+    document.getElementById("drinks").appendChild(saveHistory);
     i=i+1
 }
+
+function init () {
+    for ( let i=0; i<= 9; i++) 
+}
+
+init ()
