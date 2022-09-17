d796e2c245e32f1ca74d00695d14d17fcb328dd9
Peter Holcomb <pholcomb91@gmail.com>
2022-09-17 15:12:29 -0700

    Works, sorta

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,55 @@
 
 var listLength=document.getElementById("drinks").childElementCount
 
-
 function createFavoriteList() {
     var i=localStorage.length;
     var saveHistory=document.createElement("li");
+    saveHistory.setAttribute("id", "drink"+i);
     var drink=document.getElementById("drinksSaved").value;
     saveHistory.classList.add("listLook");
     localStorage.setItem(i, drink);
     saveHistory.innerHTML=drink;
     document.getElementById("drinks").appendChild(saveHistory);
-    i=i+1
-    listLength=listLength
-    console.log(drink)
-    console.log(listLength)
+
+    var clearEntry=document.createElement("button");
+    clearEntry.setAttribute("onclick","clearFromLocal ("+i+")");
+    clearEntry.setAttribute("id", "clear"+i);
+    clearEntry.setAttribute("type", "button");
+    clearEntry.classList.add("clearButton");
+    //clearEntry.classList.add("close-button");
+    //clearEntry.innerHTML="&times;"
+    document.getElementById("drink"+i).appendChild(clearEntry);
+    
+    
+    var clearEntryText=document.createElement("span");
+    clearEntryText.innerHTML="&times;"
+    document.getElementById("clear"+i).appendChild(clearEntryText);
+}
+
+function clearFromLocal (i) {
+    var drinkEl=document.getElementById("drink"+i);
+    drinkEl.remove();
+    localStorage.removeItem(i);
 }
 
 function init () {
-    for (var i=0; i<localStorage.length; i++) {
+    for (var i=0; i<10; i++) {
         var getSavedHistory = localStorage.getItem(i);
-        console.log(getSavedHistory);
             if (getSavedHistory !== null) {
                 var savedHistory = document.createElement("li");
+                savedHistory.setAttribute("id", "drink"+i);
                 savedHistory.classList.add("listLook");
                 savedHistory.innerHTML =getSavedHistory;
                 document.getElementById("drinks").appendChild(savedHistory);
+                var clearEntry=document.createElement("button");
+                clearEntry.setAttribute("onclick","clearFromLocal ("+i+")");
+                clearEntry.setAttribute("id", "clear"+i);
+                clearEntry.setAttribute("type", "button");
+                clearEntry.classList.add("clearButton");
+                document.getElementById("drink"+i).appendChild(clearEntry);
+                var clearEntryText=document.createElement("span");
+                clearEntryText.innerHTML="&times;"
+                document.getElementById("clear"+i).appendChild(clearEntryText);
                 } 
     }
 }
